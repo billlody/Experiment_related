21772d2c505f044ffae40f750b296ba3c5353da5
billlody <billzhengyan@hotmail.com>
2014-01-21 14:38:39 +0800

    two data subtraction

--- TempGradient.py
+++ TempGradient.py
@@ -1,45 +1,51 @@
 from differentialR import xalglib
 from differentialR import drange
+from differentialR import average_data
+from differentialR import intepolate
 from differentialR import outputfile
 
 def main():
-    FileName1='1stP-0.7V-5.516Hz-1ndTCouple-ThermalE(output).dat'
-    FileName2='1stP-0.7V-5.516Hz-2ndTCouple-ThermalE(output).dat'
+    FileName1='output1.dat'
+    FileName2='output2.dat'
     F1=open(FileName1)
     F2=open(FileName2)
     F1.readline()
     F2.readline()
 
-    T1, dT1=[], []
+    data1, data2=[], []
     while 1:
         line1=F1.readline()
         if not line1:
             break
         parts=line1.split('\t')
-        T1.append(float(parts[0]))
-        dT1.append(float(parts[1]))
+        tempererary=float(parts[0]), float(parts[1])
+        data1.append(tempererary)
 
-    T2, dT2=[], []
     while 1:
         line2=F2.readline()
         if not line2:
             break
         parts=line2.split('\t')
-        T2.append(float(parts[0]))
-        dT2.append(float(parts[1]))
+        tempererary=float(parts[0]), float(parts[1])
+        data2.append(tempererary)
 
-    p1=xalglib.polynomialfit(T1, dT1, 9)
-    p2=xalglib.polynomialfit(T2, dT2, 9)
-    T_low=max(min(T1), min(T2))
-    T_high=min(max(T1), max(T2))
-    Temperature=drange(T_low, T_high, 0.1)
+    data_1=sorted(data1, key=lambda tup: tup[0])
+    data_2=sorted(data2, key=lambda tup: tup[0])
 
-    result=[]
-    for i in range(len(Temperature)):
-        v=xalglib.barycentriccalc(p1[1], Temperature[i])-xalglib.barycentriccalc(p2[1], Temperature[i])
-        result.append(v)
+    x, y=average_data(data_1, 50)
+    data_1=zip(x, y)
+    x, y=average_data(data_2, 50)
+    data_2=zip(x,y)
 
-    outputfile(Temperature, result, result, FileName1[:17]+"(output).dat")
+    x, result=[], []
+    for t in data_2:
+        if t[0]<data_1[0][0] or t[0]>data_1[-1][0]:
+            continue
+        x.append(t[0])
+        y=intepolate(t[0], data_1)
+        result.append(t[1]-y)
+
+    outputfile(x, result, result, "HallE-output.dat")
 
 if __name__=="__main__":
     main()
